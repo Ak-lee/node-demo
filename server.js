50eac62e5ebd95c6d3765d869126aaab33d3f379
Ak-lee <295475844@qq.com>
2017-10-14 23:16:13 +0800

    我的第二次提交

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var http = require('http')
 var fs = require('fs')
 var url = require('url')
@@ -37,14 +38,14 @@ var server = http.createServer(function(request, response){
   response.write('body{background-color:#ddd;}h1{color:red;}')
   response.end()
   }
-  else if (path == '/script.js'){
+  else if (path == '/main.js'){
   response.setHeader('Content-Type','text/javascript;charset= utf-8')
   response.write('alert("这是 JS 执行的")')
   response.end()
   }
-  else if (path == '/index.html'){
+  else if (path == '/'){
   response.setHeader('Content-Type','text/html;charset= utf-8')
-  response.write('<!DOCTYPE>\n<html><head><link rel="stylesheet" href="/style.css"></head><body><h1>你好</h1><script src="/script.js"></script></body></html>')
+  response.write('<!DOCTYPE>\n<html><head><link rel="stylesheet" href="/style.css"></head><body><h1>你好</h1><script src="/main.js"></script></body></html>')
   response.end()
   }
   else{
@@ -66,5 +67,4 @@ var server = http.createServer(function(request, response){
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
 
